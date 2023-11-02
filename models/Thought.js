4bfb790f0dfb4c6177268bb274d9ce509994c2e3
Jessie Shockey <shockeyjessie92@gmail.com>
2023-11-01 22:07:14 -0600

    Create Reaction model

--- models/Thought.js
+++ models/Thought.js
@@ -1,9 +1,43 @@
 const { Schema, model } = require('mongoose');
 
+const reactionSchema = new Schema(
+{
+    reactionId:{
+        type: Schema.Type.ObjectID,
+        default: new Schema.Type.ObjectID
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        validate: {
+            validator: async function (){
+                if (!(this.reactionBody.length<=280)){
+                    return true;
+                }
+            },
+            message: props => "Thoughts must be between 1 and 280 character."
+        }
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        // TODO: ADD GETTER METHOD TO FORMAT THE TIMESTAMP ON QUERY
+    },
+},{
+    toJSON: {
+        virtuals: true,
+        },  
+}
+)
+
 const thoughtSchema = new Schema({
         thoughtText: {
             type: String,
-            required: true, 
+            required: true,
             validate: {
                 validator: async function (){
                     if (!(1<=this.thoughtText.length<=280)){
@@ -22,12 +56,7 @@ const thoughtSchema = new Schema({
             type: String,
             required: true, 
         },
-        reactions: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'reaction',
-            },
-        ]
+        reactions: [reactionSchema]
     },{
         toJSON: {
             virtuals: true,
