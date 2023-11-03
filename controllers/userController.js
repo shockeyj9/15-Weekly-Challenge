07b2512755664bfb1b900c8d6cdbabf771c7b9aa
Jessie Shockey <shockeyjessie92@gmail.com>
2023-11-02 22:33:56 -0600

    Create GET a single user by its _id and populated thought and friend data route

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,12 +3,29 @@ const {User, Thought} = require('../models');
 module.exports = {
     async getUsers(req,res){
         try {
-            const thought = await User.find();
-            res.json(thought);
+            const user = await User.find();
+            res.json(user);
         } catch (error) {
             res.status(500).json(error);
         }
     },
     
+    async getSingleUser(req,res){
+        try {
+            const user = await User.findOne(
+                {_id: req.params.userId}
+            )
+            .populate("thoughts")
+            .populate("friends")
+
+            if (!user){
+                return res.status(404).json({message: 'No user with that ID'})
+            }
+            res.json(user);
+        } catch (error) {
+            res.status(500).json(error);
+            console.log(error.message)
+        }
+    },
 
 }
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,11 +1,15 @@
 const router = require('express').Router();
 const {
     getUsers,
+    getSingleUser,
 } = require('../../controllers/userController');
 
 
 // api/users
 router.route('/').get(getUsers)
+
+// api/thoughts/:userId
+router.route('/:userId').get(getSingleUser)
 
 
 
