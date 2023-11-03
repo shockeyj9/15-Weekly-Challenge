ce147b815fd2266baed607787cae30e4c6db9e48
Jessie Shockey <shockeyjessie92@gmail.com>
2023-11-02 22:59:59 -0600

    Create Post route for new thought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -21,6 +21,27 @@ module.exports = {
             res.status(500).json(error);
         }
     },
+    async createThought(req, res) {
+        try {
+          const thought = await Thought.create(req.body);
+          const user = await User.findOneAndUpdate(
+            { _id: req.body.userId },
+            { $addToSet: { thoughts: thought._id } },
+            { new: true }
+          );
+    
+          if (!user) {
+            return res.status(404).json({
+              message: 'Thought created, but found no user with that ID',
+            })
+          }
+    
+          res.json('Created the Thought 🎉');
+        } catch (err) {
+          console.log(err);
+          res.status(500).json(err);
+        }
+      },
     async updateThought(req,res){
         try {
             const thought = await Thought.findOneAndUpdate(
