1c50b16f5af944ffbfc9348a00dc1e34089ef82b
Jessie Shockey <shockeyjessie92@gmail.com>
2023-11-07 22:34:04 -0700

    cleaned up routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -16,7 +16,7 @@ router.route('/').get(getThoughts).post(createThought)
 router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought)
 
 // api/thoughts/:thoughtId/reactions
-router.route('/:thoughtId/reactions').put(addReaction)
+router.route('/:thoughtId/reactions').post(addReaction)
 
 // api/thoughts/:thoughtId/reactions/:reactionId
 router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction)
